56df2bd046d03f679f7a090d5fe7ccf6164b55d7
Dmitriy Karpov <dmtkpv@gmail.com>
2021-03-22 11:24:06 +0200

    refactoring

--- dist/scripts.js
+++ dist/scripts.js
@@ -6,9 +6,7 @@
     // Constants
     // ----------------------
 
-    const $section = document.querySelector('section');
     const $note = document.getElementById('note');
-    const $container = document.getElementById('container');
     const $map = document.getElementById('map');
     const map = new MindMap($map, { imagesFolder: 'images/png' });
 
